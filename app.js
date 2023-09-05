8cdd70f8c53ee230fc0df373f52371666b8c15a7
Wellington Camilo <wellington.camilo@bsci.com>
2023-09-05 13:37:27 -0300

    feat added new logs | fixed time in logs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ import express from 'express';
 import fetch from 'node-fetch'; // Use node-fetch para fazer solicitações HTTP
 const app = express();
 const PORT = 3000;
-const dataAtual = new Date();
+const currentDate = new Date();
 
 const options = {
   timeZone: 'America/Sao_Paulo',
@@ -19,41 +19,58 @@ app.get('/check-site', async (req, res) => {
   const webhookURL = req.query.webhook
   const maxAttempts = 90; //Numero em minutos
   let attempts = 0;
-  let timeServerNow = dataAtual.toLocaleString('pt-BR', options);
 
-  console.log('----------- Robô de verificação -----------\n');
-  console.log('----------- Iniciado: ' + timeServerNow +  ' -----------\n');
-  async function checkSite() {
-    try {
-      
-      const response = await fetch(url);
-      if (response.status === 200) {
+  let currentDateStartServer = new Date();
+  let timeServerNow = currentDateStartServer.toLocaleString('pt-BR', options);
 
-        let timeNow = dataAtual.toLocaleString('pt-BR', options);
+  console.log('----------- Robô de verificação -- ' + timeServerNow + ' -\n');
+  console.log('URL: ' + url + '\n')
+  console.log('WEBHOOK: ' + webhookURL + '\n');
 
-        let message = 'Fim do Deploy - Ambiente de ' + environment + ' está disponível. Início do monitoramento : ' + timeNow + ' - Fim do deploy: ' + timeServerNow;
+  if(url !== undefined && url !== null && url !== ''){
 
-        
-        if (webhookURL !== undefined && webhookURL !== null && webhookURL !== ''){
-          const teamsWebhookURL = webhookURL;
-          const teamsMessage = {
-            text: message,
-          };
+    async function checkSite() {
+      try {
   
-          await fetch(teamsWebhookURL, {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(teamsMessage),
-          });
-
+        const response = await fetch(url);
+        if (response.status === 200) {
+  
+          let currentDateEndCheck = new Date();
+          let timeNow = currentDateEndCheck.toLocaleString('pt-BR', options);
+          let message = 'Fim do Deploy - Ambiente de ' + environment + ' está disponível. Início do monitoramento : ' + timeServerNow + ' - Fim do deploy: ' + timeNow;
+  
+          
+          if (webhookURL !== undefined && webhookURL !== null && webhookURL !== ''){
+            const teamsWebhookURL = webhookURL;
+            const teamsMessage = {
+              text: message,
+            };
+    
+            await fetch(teamsWebhookURL, {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify(teamsMessage),
+            });
+  
+          }
+  
+          res.status(200).json({ message: message });
+          console.log('----------- Verificação encerrada -----------\n');
+          console.log('=============================================\n');
+        } else {
+          console.log(response.status)
+          attempts++;
+          if (attempts < maxAttempts) {
+            setTimeout(checkSite, 90000);
+          } else {
+            errorHandler(environment, res)
+          }
         }
-
-        res.status(200).json({ message: message });
-        console.log('----------- Verificação encerrada -----------\n');
-      } else {
-        console.log(response.status)
+  
+  
+      } catch (error) {
         attempts++;
         if (attempts < maxAttempts) {
           setTimeout(checkSite, 90000);
@@ -61,24 +78,25 @@ app.get('/check-site', async (req, res) => {
           errorHandler(environment, res)
         }
       }
-
-
-    } catch (error) {
-      attempts++;
-      if (attempts < maxAttempts) {
-        setTimeout(checkSite, 90000);
-      } else {
-        errorHandler(environment, res)
-      }
     }
-  }
+  
+    checkSite();
 
-  checkSite();
+  } else {
+    let currentDateEndCheckError = new Date();
+    let timeNow = currentDateEndCheckError.toLocaleString('pt-BR', options);
+    let message = 'URL está vazia, não há o que verificar - ' + timeNow;
+    res.status(200).json({ message: message });
+    console.log('----------- Verificação encerrada -----------\n');
+    console.log('=============================================\n');
+  }
+  
 });
 
 async function errorHandler(environment, res) {
 
-  let timeNow = dataAtual.toLocaleString('pt-BR', options);
+  let currentDateEndCheckError = new Date();
+  let timeNow = currentDateEndCheckError.toLocaleString('pt-BR', options);
   let message = '[ERROR] - Tempo do deploy em ' + environment + ' excedeu 1 hora e 30 desde o início do monitoramento - Algo de errado ocorreu. Início: ' + timeNow;
 
   console.log(message);
@@ -89,4 +107,6 @@ async function errorHandler(environment, res) {
 
 app.listen(PORT, () => {
   console.log(`========Servidor rodando na porta ${PORT} ========\n`);
+  let timeServer = currentDate.toLocaleString('pt-BR', options);
+  console.log('----------- Iniciado: ' + timeServer +  ' -----------\n');
 });
